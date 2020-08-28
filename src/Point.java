3843adb18a4d47e3e677eb69a3c2b0b9a365bdf3
Vishal Rajpurohit <56090887+vishal7112@users.noreply.github.com>
2020-08-28 15:36:54 -0500

    New Changes

---
+++ src/Point.java
@@ -0,0 +1,43 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(){
+
+    }
+    public Point (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance () {
+        double distance = Math.sqrt((this.x)*(this.x)+(this.y)*(this.y));
+        return distance;
+    }
+
+    public double distance (int x, int y){
+        double distance = Math.sqrt((this.x - x)*(this.x - x)+(this.y - y)*(this.y - y));
+        return distance;
+    }
+
+    public double distance (Point newPoint) {
+        double distance =  distance(newPoint.getX() , newPoint.getY());
+        return distance;
+    }
+}
